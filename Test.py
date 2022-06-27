5fe3e3a049f010bea321042a8b45aacd45b52fe6
Christian Gillies <ccrg4@hotmail.com>
2022-06-27 14:28:07 -0400

    Hit Dice Extraction
    Added hitdice extractions

--- Test.py
+++ Test.py
@@ -1,5 +1,4 @@
 from  Statblock import Statblock
-
 test= Statblock("Beast_of_Ill_Omen","Beast_of_Ill_Omen.PNG")
 test.set_raw()
 """print(test.raw().splitlines())"""
@@ -9,6 +8,7 @@ test.set_raw()
 
 
 """
+print(test.raw())
 
 "Parsing variables that we take from the raw"
 creature_name = ""
@@ -18,12 +18,20 @@ alignment = ""
 armor_class = ""
 armor_type = ""
 hit_points = ""
-"Flags for different parsing protocol"
+hit_dice = ""
+
+
 name_flag = False
+"Flag for finishing name parse protocol"
 creature_info_flag=False
+"Flag for finishing creature_info parse protocol"
 armor_class_flag = False
+"Flag for finishing armor_class parse protocol"
 hit_points_flag=False
+"Flag for finishing hitpoints parse protocol"
 nextLine_flag = False 
+"Flag for telling to capture next line in addition to current"
+
 #Tells to append next line rather that set only one line
 for line in test.raw().splitlines():
     if len(line) > 2 and name_flag==False:
@@ -53,7 +61,9 @@ for line in test.raw().splitlines():
             print(line)
             creature_info_raw= line.split(" ")
             print(creature_info_raw)
-            hit_points = creature_info_raw[2]
+            hit_points = int(creature_info_raw[2])
+            for i in range(3,len(creature_info_raw)):
+                hit_dice+=creature_info_raw[i]
             hit_points_flag= True
 
 
